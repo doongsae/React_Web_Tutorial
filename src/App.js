a88b92d0b10319f9656c8c7b4791e1234a5a17eb
doongsae <doongsae@sogang.ac.kr>
2021-09-17 14:12:31 +0900

    Fix problems with styles and theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import Customer from './components/customer';
 import './App.css';
+import { Table, TableHead, TableBody, TableRow, TableCell, Paper, createTheme } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+
+const theme = createTheme();
+
+const styles = {
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+};
 
 const cust = [
 {
@@ -28,26 +43,29 @@ const cust = [
 }
 ]
 
-function App() {
+function App(props) {
+  const { classes } = props;
   return (
-    <div>
-      {
-        cust.map(cs => {
-          return (
-            <Customer
-              key={cs.id}
-              id={cs.id}
-              image={cs.image}
-              name={cs.name}
-              birthday={cs.birthday}
-              gender={cs.gender}
-              job={cs.job}
-            />
-          );
-        })
-      }
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        
+        <TableBody>
+          {cust.map(cs => {return ( <Customer key={cs.id} id={cs.id} image={cs.image} name={cs.name}
+                                     birthday={cs.birthday} gender={cs.gender} job={cs.job}/> ) })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
