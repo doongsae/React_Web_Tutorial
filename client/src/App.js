52138673d0ed38f7548a7ef3b48ffb408f198865
doongsae <doongsae@sogang.ac.kr>
2021-09-20 02:41:16 +0900

    Error on MAP

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { React, Component } from 'react';
 import Customer from './components/customer';
 import './App.css';
 import { Table, TableHead, TableBody, TableRow, TableCell, Paper, createTheme } from '@material-ui/core';
@@ -16,56 +17,49 @@ const styles = {
   }
 };
 
-const cust = [
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '일번이',
-  'birthday': '111111',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '이번이',
-  'birthday': '222222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '삼번이',
-  'birthday': '333333',
-  'gender': '남자',
-  'job': '대학생'
-}
-]
+class App extends Component {
+  state = {
+    customers: ""
+  }
 
-function App(props) {
-  const { classes } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        
-        <TableBody>
-          {cust.map(cs => {return ( <Customer key={cs.id} id={cs.id} image={cs.image} name={cs.name}
-                                     birthday={cs.birthday} gender={cs.gender} job={cs.job}/> ) })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json;
+
+    return body;
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(cs => {
+                                    return ( <Customer key={cs.id} id={cs.id} image={cs.image} name={cs.name}
+                                                        birthday={cs.birthday} gender={cs.gender} job={cs.job}/> ); })
+                                    : ""}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello, express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+          {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '일번이',
+            'birthday': '111111',
+            'gender': '남자',
+            'job': '대학생'
+          },
+          {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '이번이',
+            'birthday': '222222',
+            'gender': '남자',
+            'job': '대학생'
+          },
+          {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '삼번이',
+            'birthday': '333333',
+            'gender': '남자',
+            'job': '대학생'
+          }          
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
