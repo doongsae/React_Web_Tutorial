598dc66e9b58ac2189c24f44d9d2135d310caa58
doongsae <doongsae@sogang.ac.kr>
2021-09-27 18:20:49 +0900

    Connect with Firebase Realtime database

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const { initializeApp } = require('@firebase/app');
+const { getDatabase, ref, onValue } = require('@firebase/database');
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,33 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+var firebaseConfig = {
+  apiKey: conf.apiKey,
+  authDomain: conf.authDomain,
+  databaseURL: conf.databaseURL,
+  storageBucket: conf.storageBucket
+};
+
+const fbApp = initializeApp(firebaseConfig);
+var fb_database = getDatabase();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-          {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '일번이',
-            'birthday': '111111',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '이번이',
-            'birthday': '222222',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '삼번이',
-            'birthday': '333333',
-            'gender': '남자',
-            'job': '대학생'
-          }          
-    ]);
+  const personRef = ref(fb_database, '/people');
+  onValue(personRef, (snapshot) => {
+    const resV = snapshot.val();
+    res.send(resV);
+  })
 });
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
